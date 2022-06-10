55d4a5e0d704b916276967facd9874ddc281f749
Abhishek Pandey Lalan <abhishekp2917@gmail.com>
2022-06-10 23:27:22 +0530

    Change 'var' to 'let' in indexJS.js

--- indexJS.js
+++ indexJS.js
@@ -1,11 +1,10 @@
 // opening menu
 const openMenu = (element, index) => {
 
-    var option = document.querySelectorAll("#menu div div div")
-    var menu = document.querySelectorAll("#menu div ul")
-    var i
+    let option = document.querySelectorAll("#menu div div div")
+    let menu = document.querySelectorAll("#menu div ul")
 
-    for(i=0; i<option.length; ++i){
+    for(let i=0; i<option.length; ++i){
         option[i].classList.remove("menu-option-active")
         menu[i].classList.remove("menu-active")
     }
