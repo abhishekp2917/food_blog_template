83c30277d8c9d7598b7dc0d327d949eb79110aa5
Abhishek Pandey Lalan <abhishekp2917@gmail.com>
2022-06-10 23:18:40 +0530

    Update index.html
    Reposition javascript script link from head to below footer

--- indexJS.js
+++ indexJS.js
@@ -1,15 +1,15 @@
 // opening menu
-const openMenu = (element,index) => {
+const openMenu = (element, index) => {
 
-    var option = document.querySelectorAll("#menu div div div");
-    var menu = document.querySelectorAll("#menu div ul");
-    var i; 
+    var option = document.querySelectorAll("#menu div div div")
+    var menu = document.querySelectorAll("#menu div ul")
+    var i
 
     for(i=0; i<option.length; ++i){
-        option[i].classList.remove("menu-option-active");
-        menu[i].classList.remove("menu-active");
+        option[i].classList.remove("menu-option-active")
+        menu[i].classList.remove("menu-active")
     }
 
-    element.classList.add("menu-option-active");
-    menu[index].classList.add("menu-active");
+    element.classList.add("menu-option-active")
+    menu[index].classList.add("menu-active")
 }
