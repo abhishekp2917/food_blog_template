d9cb64de64d2144e4a72c5ba113a50234383fd1a
Abhishek Pandey Lalan <abhishekp2917@gmail.com>
2022-06-10 22:58:16 +0530

    Initial commit

---
+++ indexJS.js
@@ -0,0 +1,15 @@
+// opening menu
+function openMenu(element,index){
+
+    var option = document.querySelectorAll("#menu div div div");
+    var menu = document.querySelectorAll("#menu div ul");
+    var i; 
+
+    for(i=0; i<option.length; ++i){
+        option[i].classList.remove("menu-option-active");
+        menu[i].classList.remove("menu-active");
+    }
+
+    element.classList.add("menu-option-active");
+    menu[index].classList.add("menu-active");
+}
